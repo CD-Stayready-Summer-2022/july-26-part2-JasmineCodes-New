0256fdbf316d9a88b6e531348c56bc170aaa3dd8
JasmineCodes-New <jasmine.irvin@ymail.com>
2022-07-26 10:41:38 -0400

    finished

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,7 @@
 package cd.get.ready.algorithms;
 
+import java.util.HashMap;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,7 +9,23 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
-        return null;
+        HashMap<String, Integer> mapInput = new HashMap<String, Integer>();
+        int duplicateCount = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if(!mapInput.containsKey(input[i])) {
+                mapInput.put(input[i],1);
+            }
+            else {
+                Integer currentValueAtKey = mapInput.get(input[i]);
+                if(currentValueAtKey == 1) {
+                    duplicateCount += 1;
+                }
+                currentValueAtKey++;
+                mapInput.put(input[i], currentValueAtKey);
+            }
+        }
+        return duplicateCount;
     }
 
     /**
@@ -16,6 +34,22 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(Integer[] input){
-        return null;
+        HashMap<Integer, Integer> mapInput = new HashMap<Integer, Integer>();
+        int duplicateCount = 0;
+
+        for (int i = 0; i < input.length; i++) {
+            if(!mapInput.containsKey(input[i])) {
+                mapInput.put(input[i],1);
+            }
+            else {
+                Integer currentValueAtKey = mapInput.get(input[i]);
+                if(currentValueAtKey == 1) {
+                    duplicateCount += 1;
+                }
+                currentValueAtKey++;
+                mapInput.put(input[i], currentValueAtKey);
+            }
+        }
+        return duplicateCount;
     }
 }
